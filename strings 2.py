55f6ff9379a566ab69185fd43e05f093bc4b0ab4
rehan sayed <sayedrehan3333@gmail.com>
2019-03-26 15:48:45 +0530

    first commit

---
+++ strings 2.py
@@ -0,0 +1,11 @@
+mobin="""SHE Mobin BTBs"""
+print(len(mobin))
+print(mobin.upper())
+print(mobin.lower())
+print(mobin.count('B'))
+print(mobin.find('n'))
+AR = mobin.replace('BTBs','BTS')
+print(AR)
+SALMAN ='super salman'
+pasha = '{} and {}'.format(AR, SALMAN)
+print(pasha)
