c8d03f757b3278eccaa2e346f85b460cc25b523c
manzars <manzarshaikh69@gmail.com>
2019-04-01 15:15:46 +0530

    tutorial2

---
+++ integers and floats.py
@@ -0,0 +1,27 @@
+# airthmrtic operators:
+# +,-,*,/,//,**,%
+num = 1
+# print(type(num))
+# print(7 *(10 /   5))
+num = num +2.5
+# print(num)
+num = num * -num
+print(num)
+print(abs(num))
+print(round(num))
+print(int(num))
+
+
+# comparisons:
+# ==,!=,>,<,<=,>=
+num_2 =5
+# print(num != num_2)
+
+num_3 = '50'
+num_4 = '100'
+print(num_3 + num_4)
+print(type(num_3))
+
+num_3 = int(num_3)
+num_4 = int(num_4)
+print(num_3 + num_4)
